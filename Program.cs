b20296bc3981aa829fae2168e360e2cab1153b21
Josiah Aubin <josiahaubin@gmail.com>
2019-10-02 16:32:54 -0600

    init

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace guess
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Random rand = new Random();
+      int randomNum = rand.Next(1, 101);
+
+      bool running = true;
+
+      while (running)
+      {
+        try
+        {
+          Console.WriteLine("Pick a number between 1-100.");
+          string userNum = Console.ReadLine();
+
+          if (int.TryParse(userNum, out int userInt))
+          {
+            if (userInt == randomNum)
+            {
+              Console.WriteLine("Congrats, that is correct!");
+              Console.Beep();
+              running = false;
+            }
+            else if (userInt > randomNum)
+            {
+              Console.WriteLine("Too high, guess again");
+            }
+            else
+            {
+              Console.WriteLine("Too low, try again");
+            }
+          }
+
+        }
+        catch (Exception e)
+        {
+          Console.WriteLine(e);
+        }
+      }
+    }
+  }
+}
