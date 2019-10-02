cbc4386329b5d441dd77a8a6d643c316ec15fe91
Josiah Aubin <josiahaubin@gmail.com>
2019-10-02 17:00:07 -0600

    Replay works

--- Program.cs
+++ Program.cs
@@ -23,16 +23,30 @@ namespace guess
             if (userInt == randomNum)
             {
               Console.WriteLine("Congrats, that is correct!");
-              Console.Beep();
-              running = false;
+              Console.WriteLine("Do you want to play again?");
+              string answer = Console.ReadLine().ToLower();
+
+              switch (answer)
+              {
+                case "yes":
+                  running = true;
+                  randomNum = rand.Next(1, 101);
+                  break;
+
+                case "no":
+                  running = false;
+                  break;
+              }
             }
             else if (userInt > randomNum)
             {
               Console.WriteLine("Too high, guess again");
+              Console.Beep();
             }
             else
             {
               Console.WriteLine("Too low, try again");
+              Console.Beep();
             }
           }
 
